74bc721316983c4f3e03104e0e44790b16295bfa
firdavs7755 <firdavs0603@gmail.com>
2021-05-12 10:56:59 +0500

    ersgetbdf

--- src/cabinet/settings/account/index.js
+++ src/cabinet/settings/account/index.js
@@ -6,6 +6,9 @@ import {connect} from "react-redux";
 import {Button, Form, Input, Radio} from "antd";
 import {CheckCircleOutlined, PlusCircleOutlined} from "@ant-design/icons";
 import st from './acc.module.scss'
+import save from '../../../assests/settings/save.svg'
+import cancel from '../../../assests/settings/cancel.svg'
+
 function Account({userKey}) {
     return(
         <>
@@ -15,94 +18,134 @@ function Account({userKey}) {
                 // onFinish={handleKeySubmit}
                 scrollToFirstError
             >
-                <Form.Item name='u_key'>
-                    <Radio.Group style={{width:'65%'}}>
-                        <Radio >
-                            <div>
-                                <div className='flexible'>
-                                    <div>
-                                        <div className={'flexible'}>
-                                            <p className={'title'}>INN:</p>
-                                            <p>{userKey.username}</p>
+                <div className={st.block}>
+                    <Form.Item name='u_key'>
+                        <Radio.Group style={{width:'65%'}}>
+                            <Radio >
+                                <div>
+                                    <div className='flexible'>
+                                        <div>
+                                            <div className={'flexible'}>
+                                                <p className={'title'}>INN:</p>
+                                                <p>{userKey.username}</p>
+                                            </div>
+                                            <div className={'flexible'}>
+                                                <p className={'title'}>Organization:</p>
+                                                <p>{userKey.username}</p>
+                                            </div>
                                         </div>
-                                        <div className={'flexible'}>
-                                            <p className={'title'}>Organization:</p>
-                                            <p>{userKey.username}</p>
-                                        </div>
-                                    </div>
-                                    <div style={{marginLeft:'40px'}}>
-                                        <div className={'flexible'}>
-                                            <p className={'title'}>F.I.O</p>
-                                            <p>{userKey.fio}</p>
-                                        </div>
-                                        <div className={'flexible'}>
-                                            <p className={'title'}>Amal qilish muddati</p>
-                                            <p>2222.22.22</p>
+                                        <div style={{marginLeft:'40px'}}>
+                                            <div className={'flexible'}>
+                                                <p className={'title'}>F.I.O</p>
+                                                <p>{userKey.fio}</p>
+                                            </div>
+                                            <div className={'flexible'}>
+                                                <p className={'title'}>Amal qilish muddati</p>
+                                                <p>2222.22.22</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </Radio>
-                    </Radio.Group>
-                </Form.Item>
+                            </Radio>
+                        </Radio.Group>
+                    </Form.Item>
 
-                <div className={'flexible'} style={{marginTop:'6%'}}>
-                    <div style={{width:'47%'}}>
-                        <Form.Item>
-                            <Form.Item
-                                rules={[{required: true}]}
-                                // key="dyna-form-facutura-no"
-                                name="inn">
-                                <Input
-                                    size="large"
-                                    placeholder="INN" />
+                    <div className={'flexible'} style={{marginTop:'24px'}}>
+                        <div style={{width:'48.5%'}}>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>Manzil</p></span>
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="address">
+                                    <Input
+                                        size="large"
+                                        placeholder="Manzil" />
+                                </Form.Item>
                             </Form.Item>
-                            <span className="custom-input-label-1"><p className={'label'}>INN</p></span>
-                        </Form.Item>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>Viloyat</p></span>
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="address">
+                                    <Input
+                                        size="large"
+                                        placeholder="Viloyat" />
+                                </Form.Item>
+                            </Form.Item>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>OKED</p></span>
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="inn">
+                                    <Input
+                                        size="large"
+                                        placeholder="INN" />
+                                </Form.Item>
+                            </Form.Item>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>MFO</p></span>
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="address">
+                                    <Input
+                                        size="large"
+                                        placeholder="Address" />
+                                </Form.Item>
+                            </Form.Item>
+                        </div>
 
-                        <Form.Item style={{marginTop:'7%'}}>
-                            <Form.Item
-                                rules={[{required: true}]}
-                                name="address">
-                                <Input
-                                    size="large"
-                                    placeholder="Address" />
-                            </Form.Item>
-                            <span className="custom-input-label-1"><p className="label">Address</p></span>
-                        </Form.Item>
-                    </div>
+                        <div style={{width:'3%'}}></div>
 
-                    <div style={{width:'6%'}}></div>
+                        <div style={{width:'48.5%'}}>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>Director</p></span>
 
-                    <div style={{width:'47%'}}>
-                        <Form.Item>
-                            <Form.Item
-                                rules={[{required: true}]}
-                                // key="dyna-form-facutura-no"
-                                name="director">
-                                <Input
-                                    size="large"
-                                    placeholder="Director" />
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="director">
+                                    <Input
+                                        size="large"
+                                        placeholder="Director" />
+                                </Form.Item>
                             </Form.Item>
-                            <span className="custom-input-label-1"><p className="label">Director</p></span>
-                        </Form.Item>
-                        <Form.Item style={{marginTop:'7%'}}>
-                            <Form.Item
-                                rules={[{required: true}]}
-                                // key="dyna-form-facutura-no"
-                                name="number">
-                                <Input
-                                    size="large"
-                                    placeholder="Account number" />
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>Hisob raqam</p></span>
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="number">
+                                    <Input
+                                        size="large"
+                                        placeholder="Hisob raqam" />
+                                </Form.Item>
                             </Form.Item>
-                            <span className="custom-input-label-1"><p className="label">Account number</p></span>
-                        </Form.Item>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>Nomi</p></span>
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    name="director">
+                                    <Input
+                                        size="large"
+                                        placeholder="Director" />
+                                </Form.Item>
+                            </Form.Item>
+                            <Form.Item>
+                                <span className="custom-input-label-1"><p className={st.label}>Hisobchi</p></span>
+
+                                <Form.Item
+                                    rules={[{required: true}]}
+                                    // key="dyna-form-facutura-no"
+                                    name="number">
+                                    <Input
+                                        size="large"
+                                        placeholder="Account number" />
+                                </Form.Item>
+                            </Form.Item>
+                        </div>
                     </div>
                 </div>
-                {console.log('user',userKey)}
-                <div className={'flexible'}>
-                    <Button className={st.save}><CheckCircleOutlined />Сохранить</Button>
-                    <Button className={st.cancel}><PlusCircleOutlined className={`${st.delete_icon} `} />Cancel</Button>
+                <div className={'flexible'} style={{marginTop:'28px'}}>
+                    <Button className={st.save}><img src={save} alt=""/>Сохранить</Button>
+                    <Button className={st.cancel}><img src={cancel} alt=""/>Отменить</Button>
                 </div>
             </Form>
         </>
